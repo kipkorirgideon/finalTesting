4a50698985b1ee5da42b028b630e30fe71d7e0f0
kipkorirgideon <korirgideon29@gmail.com>
2021-06-07 14:34:12 +0300

    Fifth commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1>working on master branch</h1>
       <h1>committing for the second time</h1>
       <h1>Fourth commit</h1>
+      <h2>Fifth committing</h2>
       
     </div>
   );
